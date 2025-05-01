38b2ec1e0c6a0c59a8e53668b0c013056569b34e
tosuke <13393900+tosuke@users.noreply.github.com>
2025-05-02 00:47:26 +0900

    feat: update IAM roles for service accounts

--- terraform/gcloud/project-iam.tf
+++ terraform/gcloud/project-iam.tf
@@ -1,15 +1,22 @@
 # IAM関連の権限
+# IAM 操作権限
+resource "google_project_iam_member" "plan_sa_iam_reviewer" {
+  project = local.project_id
+  role    = "roles/iam.securityReviewer"
+  member  = "serviceAccount:${google_service_account.gha_terraform_plan.email}"
+}
+
+resource "google_project_iam_member" "apply_sa_iam_admin" {
+  project = local.project_id
+  role    = "roles/iam.securityAdmin"
+  member  = "serviceAccount:${google_service_account.gha_terraform_apply.email}"
+}
+
 ## Workload Identity Pool関連の権限
 resource "google_project_iam_member" "plan_sa_workload_identity_pool_viewer" {
   project = local.project_id
   role    = "roles/iam.workloadIdentityPoolViewer"
   member  = "serviceAccount:${google_service_account.gha_terraform_plan.email}"
-}
-
-resource "google_project_iam_member" "apply_sa_workload_identity_pool_viewer" {
-  project = local.project_id
-  role    = "roles/iam.workloadIdentityPoolViewer"
-  member  = "serviceAccount:${google_service_account.gha_terraform_apply.email}"
 }
 
 resource "google_project_iam_member" "apply_sa_workload_identity_pool_admin" {
@@ -41,12 +48,5 @@ resource "google_project_iam_member" "plan_sa_storage_bucket_viewer" {
 resource "google_project_iam_member" "apply_sa_storage_admin" {
   project = local.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.gha_terraform_apply.email}"
-}
-
-# プロジェクト全般の権限
-resource "google_project_iam_member" "apply_sa_project_editor" {
-  project = local.project_id
-  role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.gha_terraform_apply.email}"
 }
--- terraform/gcloud/tfstate-bucket.tf
+++ terraform/gcloud/tfstate-bucket.tf
@@ -2,8 +2,8 @@ data "google_storage_bucket" "tf_backend" {
   name = "tosuke-homelab-tfstate"
 }
 
-resource "google_storage_bucket_iam_member" "tf_backend_plan_sa_object_viewer" {
+resource "google_storage_bucket_iam_member" "tf_backend_plan_sa_object_user" {
   bucket = data.google_storage_bucket.tf_backend.name
-  role   = "roles/storage.objectViewer"
+  role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.gha_terraform_plan.email}"
 }
