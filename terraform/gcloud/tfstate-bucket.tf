85696c0bd5a97d6035a85fac8f60869fba841041
tosuke <13393900+tosuke@users.noreply.github.com>
2025-05-01 23:56:58 +0900

    fix: update IAM roles for Terraform service accounts to use objectUser

--- terraform/gcloud/tfstate-bucket.tf
+++ terraform/gcloud/tfstate-bucket.tf
@@ -2,14 +2,14 @@ data "google_storage_bucket" "tf_backend" {
   name = "tosuke-homelab-tfstate"
 }
 
-resource "google_storage_bucket_iam_member" "tf_backend_plan_sa_object_viewer" {
+resource "google_storage_bucket_iam_member" "tf_backend_plan_sa_object_user" {
   bucket = data.google_storage_bucket.tf_backend.name
-  role   = "roles/storage.objectViewer"
+  role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.gha_terraform_plan.email}"
 }
 
-resource "google_storage_bucket_iam_member" "tf_backend_apply_sa_object_admin" {
+resource "google_storage_bucket_iam_member" "tf_backend_apply_sa_object_user" {
   bucket = data.google_storage_bucket.tf_backend.name
-  role   = "roles/storage.objectAdmin"
+  role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.gha_terraform_apply.email}"
 }
